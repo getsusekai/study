a4788a4d5590e1591a7ad5751fdf62f41a9210a7
getsusekai <05december97@gmail.com>
2020-08-31 16:37:37 +0400

    completed arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = 0
+        array.each { |el| el > max ? max = el : max }
+        array.map! { |el| el.positive? ? max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        low = 0
+        high = array.length - 1
+        mid = array.length / 2
+        while low <= high
+          if array[mid] < query
+            low = mid + 1
+          elsif array[mid] > query
+            high = mid - 1
+          end
+          mid = low + (high - low) / 2
+          return mid if array[mid] == query
+        end
+        -1
       end
     end
   end
