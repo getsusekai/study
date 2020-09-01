d4291380529dd33c98f68ab63530424faedb0098
getsusekai <05december97@gmail.com>
2020-09-01 18:35:07 +0400

    completed fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,15 +1,25 @@
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        sum = 0
+        filtered_array = array.select do |film|
+          (film['rating_kinopoisk'].to_f != 0) &&
+            (!film['country'].nil? && film['country'].include?(','))
+        end
+        filtered_array.each do |film|
+          sum += film['rating_kinopoisk'].to_f
+        end
+        sum / filtered_array.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        char_count = 0
+        filtered_films = films.select { |film| (film['rating_kinopoisk'].to_f >= threshold) }
+        filtered_films.each do |film|
+          char_count += film['name'].count('и')
+        end
+        char_count
       end
     end
   end
