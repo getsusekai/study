ecb14ec8e6d169575e8438fad85b9be9715a8c25
getsusekai <05december97@gmail.com>
2020-09-15 14:41:45 +0400

    completed rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,28 @@
 module Inatra
   class << self
-    def routes(&block); end
+    def routes(&block)
+      @buffer = {}
+      instance_eval(&block)
+    end
 
-    def call(env); end
+    def call(env)
+      request_method = env['REQUEST_METHOD'].to_s.downcase
+      path_info = env['PATH_INFO'].to_s.downcase
+      @buffer[request_method][path_info] ? @buffer[request_method][path_info].call : [404, {}, 'Not Found']
+    end
+
+    def method_missing(method, *path, &block)
+      if path.size == 1
+        method = method.to_s.downcase
+        @buffer[method] ||= {}
+        @buffer[method][path[0]] = block
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method, *_path)
+      @buffer.include?(method.to_s.downcase)
+    end
   end
 end
